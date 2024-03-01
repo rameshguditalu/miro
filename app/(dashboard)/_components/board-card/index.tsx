257298960ef3c56c4752eb29d6ef71436169d568
Ramesh176 <rameshsw17@gmail.com>
2024-03-01 17:59:43 +0530

    Added Delete and Rename Functionality for boards

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -8,6 +8,8 @@ import { toast } from "sonner";
 import { formatDistanceToNow } from "date-fns";
 import { Footer } from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Actions } from "@/components/actions";
+import { MoreHorizontal } from "lucide-react";
 
 interface BoardCardProps {
   id: string;
@@ -50,11 +52,11 @@ export const BoardCard = ({
         <div className="relative flex-1 bg-amber-50">
           <Image src={imageUrl} alt={title} fill className="object-fit" />
           <Overlay />
-          {/* <Actions id={id} title={title} side="right">
+          <Actions id={id} title={title} side="right">
             <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
               <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
             </button>
-          </Actions> */}
+          </Actions>
         </div>
         <Footer
           isFavorite={isFavorite}

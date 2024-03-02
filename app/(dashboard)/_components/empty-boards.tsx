167a8d4505a514e273159f37d8be7c32dd2cf2aa
Ramesh176 <rameshsw17@gmail.com>
2024-03-02 19:20:48 +0530

    Added Boards Page and liveblocks initialization

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -7,8 +7,10 @@ import Image from "next/image";
 import { useOrganization } from "@clerk/nextjs";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export const EmptyBoards = () => {
+  const router = useRouter();
   const { organization } = useOrganization();
   const { mutate, pending } = useApiMutation(api.board.create);
 
@@ -21,6 +23,7 @@ export const EmptyBoards = () => {
     })
       .then((id) => {
         toast.success("Board Created");
+        router.push(`board/${id}`);
       })
       .catch(() => toast.error("Failed to create a board"));
   };
---
+++ app/api/liveblocks-auth/route.ts
@@ -0,0 +1,40 @@
+import { Liveblocks } from "@liveblocks/node";
+import { ConvexHttpClient } from "convex/browser";
+import { auth, currentUser } from "@clerk/nextjs";
+
+import { api } from "@/convex/_generated/api";
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const liveblocks = new Liveblocks({
+  secret:
+    "sk_dev_xaWkSg2fguGxnD-Bqw-wDI2PILSPizVFY2_Z534Mli3sqO5o2myr5YI2hfc4nxA7",
+});
+
+export async function POST(request: Request) {
+  const authorization = await auth();
+  const user = await currentUser();
+
+  if (!authorization || !user) {
+    return new Response("Unauthorized", { status: 403 });
+  }
+
+  const { room } = await request.json();
+  const board = await convex.query(api.board.get, { id: room });
+
+  if (board?.orgId !== authorization.orgId) {
+    return new Response("Unauthorized", { status: 403 });
+  }
+
+  const userInfo = {
+    name: user.firstName || "Teammeate",
+    picture: user.imageUrl,
+  };
+
+  const session = liveblocks.prepareSession(user.id, { userInfo });
+
+  if (room) {
+    session.allow(room, session.FULL_ACCESS);
+  }
+
+  const { status, body } = await session.authorize();
+  return new Response(body, { status });
+}
